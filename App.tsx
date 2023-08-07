75540146125d21aa982f1db3ff501bd2c8ae8bd2
jj <yobomedas@Jaskier>
2023-08-07 22:31:41 +0200

    horizontal scrollView

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef} from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  StyleSheet,
+  View,
+  Animated,
+  useWindowDimensions,
+} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Pages = {
+  
 }
+
+const App = () => {
+  const scrollX = useRef(new Animated.Value(0)).current
+  const {width: windowWidth} = useWindowDimensions()
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.scrollContainer}>
+        <ScrollView
+          horizontal={true}
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: scrollX,
+                },
+              },
+            },
+          ], {useNativeDriver: false})}
+          scrollEventThrottle={1}>
+          {[1, 2, 3].map((pages, pageIndex) => {
+            return (
+              <View style={{width: windowWidth, height: 100}} key={pageIndex}>
+                <View style={{ marginLeft: 40, marginRight: 40 }}>               
+                  <Text>Hello</Text>
+                </View>
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#aaa',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  scrollContainer: {
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
+
+export default App
